8ce1774a47cd78642e8c1369898e83b49b4867cd
sebastianto <sebastt.93@gmail.com>
2021-05-04 01:11:00 +1000

    Interfaces done, all styles added, img webpack set up done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 
 import '../assets/styles/components/Header.scss'
 
+import logo from "../assets/static/logo-platzi-video-BW2.png";
+import useIcon from "../assets/static/user-icon.png"
+
 const Header = () => {
   return (
     <header className="header">
-    <img className="header__img" src='' alt="Platzi Video" />
+    <img className="header__img" src={logo} alt="Platzi Video" />
     <div className="header__menu">
       <div className="header__menu--profile">
-        <img src='' alt="" />
+        <img src={useIcon} alt="" />
         <p>Perfil</p>
       </div>
       <ul>
--- src/containers/App.js
+++ src/containers/App.js
@@ -14,11 +14,24 @@ const App = () => (
     <Header />
     <Search />
 
-    <Categories>
+    <Categories title="Mi lista">
       <Carousel>
         <CarouselItem />
         <CarouselItem />
         <CarouselItem />
+        <CarouselItem />
+      </Carousel>
+    </Categories>
+
+    <Categories title="Tendencias">
+      <Carousel>
+        <CarouselItem />
+        <CarouselItem />
+      </Carousel>
+    </Categories>
+
+    <Categories title="Originales de platzi video">
+      <Carousel>
         <CarouselItem />
       </Carousel>
     </Categories>
